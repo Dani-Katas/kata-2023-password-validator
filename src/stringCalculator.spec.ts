4771c3e344892048812debbbad3cacd8bc2f142d
Daniel Ramos <danielramosacosta1@gmail.com>
2023-03-10 14:53:10 +0000

    works with two numbers refactored

--- src/stringCalculator.spec.ts
+++ src/stringCalculator.spec.ts
@@ -1,11 +1,6 @@
 import { describe, expect, it } from "vitest"
 import { stringCalculator } from "./stringCalculator.js"
 
-// stringCalculator(numbers: string): number
-// stringCalculator("1,2") =>> 3
-// stringCalculator("1") =>> 1
-// stringCalculator("") =>> 0 check
-
 describe("stringCalculator", () => {
   describe("first iteration", () => {
     it("returns zero when empty string", () => {
